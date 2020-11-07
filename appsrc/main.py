489a54130bc94c7cacf0ab54521fa36b3ad10ac9
JohnZhong <0429zhongchen@gmail.com>
2020-11-06 22:12:15 -0500

    implement page

--- appsrc/main.py
+++ appsrc/main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, jsonify, session
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
+TABLIST = ["tab1", "tab2"]
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def homepage():
-    return render_template("main.html")
+    return render_template("index.html", tablist=TABLIST)
+
+@app.route('/tab1')
+def tab1():
+    return render_template("tab1.html", tablist=TABLIST)
+
+@app.route('/tab2')
+def tab2():
+    return render_template("tab2.html", tablist=TABLIST)
 
 
 if __name__ == "__main__":
