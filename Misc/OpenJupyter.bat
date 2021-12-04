261f9384746c180399c35bd40dac412eb335b0c2
JohnZhong <0429zhongchen@gmail.com>
2021-12-03 23:06:48 -0500

    update

--- Misc/OpenJupyter.bat
+++ Misc/OpenJupyter.bat
@@ -1,6 +1,33 @@
 :::: Open Jupyter Notebook in current directory
-set TARGET_DIR=%~dp0
+
+@echo off
+
+if exist "%HOMEDRIVE%%HOMEPATH%\Anaconda3" (
+    set "ANACONDA_PATH=%HOMEDRIVE%%HOMEPATH%\Anaconda3"
+    goto check_anaconda
+)
+if exist "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Continuum\Anaconda3" (
+    set "ANACONDA_PATH=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Continuum\Anaconda3"
+    goto check_anaconda
+)
+if exist  "%HOMEDRIVE%\ProgramData\Anaconda3\" (
+    set "ANACONDA_PATH=%HOMEDRIVE%\ProgramData\Anaconda3"
+    goto check_anaconda
+)
+if exist  "%HOMEDRIVE%\Program Files\Anaconda3\" (
+    set "ANACONDA_PATH=%HOMEDRIVE%\Program Files\Anaconda3"
+    goto check_anaconda
+)
+rem Promp the user if we cannot find Anaconda in the proper place
+set /P ANACONDA_PATH="Cannot find Anaconda, please insert path to Anaconda3:"
+
+:check_anaconda
+echo AnacondaPath:%ANACONDA_PATH%
+
+set ANACONDA_SCRIPTS_PATH="%ANACONDA_PATH%"\Scripts
+set THIS_SCRIPT_DIR=%~dp0
 
 :::: Anaconda Env Path
-call "C:\Users\0429z\Anaconda3\Scripts\activate.bat"
-jupyter notebook --notebook-dir %TARGET_DIR%
+call %ANACONDA_SCRIPTS_PATH%\activate.bat
+jupyter notebook --notebook-dir %THIS_SCRIPT_DIR%
+pause
