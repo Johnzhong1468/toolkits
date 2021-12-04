8d977b70dca9134d0b68fad9b77dde138e10f255
JohnZhong <0429zhongchen@gmail.com>
2021-12-04 13:07:53 -0500

    add logger

--- src/definitions.py
+++ src/definitions.py
@@ -3,3 +3,5 @@ import datetime
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 LOCAL_TZ = datetime.datetime.now(datetime.timezone(datetime.timedelta(0))).astimezone().tzinfo
+TEMP_DIR = os.path.join(ROOT_DIR, "temp")
+LOGS_DIR = os.path.join(ROOT_DIR, "logs")
---
+++ src/lib/log_util.py
@@ -0,0 +1,99 @@
+import datetime
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import json
+from src.definitions import LOGS_DIR
+
+
+class ProjectLogger:
+
+    DEFAULT = "src"
+    LOGS_ENABLED = True
+    CONSOLE_PRINT_ENABLED = True
+    REALTIME_WRITING_ENABLED = False
+    REALTIME_WRITING_PATH = None
+    FILE_BACKUP_COUNT = 60
+    LOG_FORMAT = "[%(asctime)s] %(levelname)s - %(message)s"
+
+    LOGGERS = {}
+    LOG_DATA = []
+    LOG_TIME = []
+
+    def __init__(self, name = None):
+        self.log_data = []
+        self.log_time = []
+        self.logger_name = self.DEFAULT if name is None else name
+        self.logger_obj = self.log_set_up(self.logger_name)
+
+    @classmethod
+    def get_logger(cls, name):
+        return cls(name)
+
+    @property
+    def default_logger(self):
+        return ProjectLogger(self.DEFAULT)
+
+    @staticmethod
+    def log_set_up(log_name):
+        if ProjectLogger.LOGGERS.get(log_name):
+            return ProjectLogger.LOGGERS.get(log_name)
+        logger = logging.getLogger(log_name)
+        log_file = f"{log_name}.log"
+        if log_name == ProjectLogger.DEFAULT:
+            log_dir = LOGS_DIR
+        else:
+            log_dir = os.path.join(LOGS_DIR, log_name)
+        os.makedirs(log_dir, exist_ok=True)
+        log_path = os.path.join(log_dir, log_file)
+        handler = TimedRotatingFileHandler(log_path, when="midnight", backupCount=ProjectLogger.FILE_BACKUP_COUNT)
+        handler.setFormatter(logging.Formatter(ProjectLogger.LOG_FORMAT))
+
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+        ProjectLogger.LOGGERS[log_name] = logger
+        return logger
+
+    def logger(self, *log_strings, level="INFO", print_stdout=True):
+        log_string = " ".join([str(l) for l in log_strings])
+        if ProjectLogger.LOGS_ENABLED:
+            getattr(self.logger_obj, level.lower())(log_string)
+            if self.logger_name != self.DEFAULT:
+                getattr(self.default_logger.logger_obj, level.lower())(log_string)
+        if self.CONSOLE_PRINT_ENABLED and print_stdout:
+            print(log_string)
+
+        self.log_data.append(log_string)
+        ProjectLogger.LOG_DATA.append(self.log_data[-1])
+        self.log_time.append(str(datetime.datetime.now()))
+        ProjectLogger.LOG_TIME.append(self.log_time[-1])
+
+        if ProjectLogger.REALTIME_WRITING_ENABLED and ProjectLogger.REALTIME_WRITING_PATH is not None:
+            if os.path.exists(ProjectLogger.REALTIME_WRITING_PATH):
+                with open(ProjectLogger.REALTIME_WRITING_PATH) as json_log:
+                    data = json.load(json_log)
+            else:
+                data = {}
+            data[self.log_time[-1]] = self.log_data[-1]
+
+            with open(ProjectLogger.REALTIME_WRITING_PATH, "w") as json_log:
+                json.dump(data, json_log)
+
+    @staticmethod
+    def flush_logs():
+        ProjectLogger.LOG_DATA = []
+        ProjectLogger.LOG_TIME = []
+
+    @staticmethod
+    def to_json():
+        return dict(zip(ProjectLogger.LOG_TIME, ProjectLogger.LOG_DATA))
+
+    @staticmethod
+    def write_to_file(directory):
+        file_name = f"LogRequest_{str(datetime.datetime.now())}.json"
+        os.makedirs(directory, exist_ok=True)
+        with open(os.path.join(directory, file_name), "w") as log_file:
+            json.dump(ProjectLogger.to_json(), log_file)
+
+
+LOG = ProjectLogger()
